aea0f7d7ebc4abb4e7b370616f9431ba06d59b23
Caleb Barnard <poisonpsychicghost@gmail.com>
2026-05-31 16:37:05 -0400

    Updated Weather Banner to include new API data.

--- src/components/WeatherBanner.vue
+++ src/components/WeatherBanner.vue
@@ -22,19 +22,26 @@ watch(() => props.dateStr, fetchWeather)
 
 <template>
   <div v-if="weather && mode === 'monthly'">
-    <span>Avg: {{ Math.round(weather.temp) }}°F</span>
+    <span>Avg: {{ Math.round(weather.avgtemp_f) }}°F</span>
     <img :src="weather.icon" alt="" v-if="weather.icon" />
   </div>
   <div v-else-if="weather && mode === 'weekly'">
-    <span>Avg: {{ Math.round(weather.temp) }}°F</span>
     <span>{{ weather.condition }}</span>
     <img :src="weather.icon" alt="" v-if="weather.icon" />
+    <span v-if="!isNaN(weather.temp_min_f) && !isNaN(weather.temp_max_f)">Temp: {{ Math.round(weather.temp_min_f) }} - {{ Math.round(weather.temp_max_f) }}°F</span>
+    <span v-if="!isNaN(weather.humidity)">Humidity: {{ Math.round(weather.humidity) }}%</span>
+    <span v-if="!isNaN(weather.daily_chance_of_rain)">Rain Chance: {{ Math.round(weather.daily_chance_of_rain) }}%</span>
+
   </div>
   <div v-else-if="weather && mode === 'daily'">
-    <span v-if="!isNaN(weather.temp_current)">Current Temp: {{ Math.round(weather.temp_current) }}°F</span>
-    <span v-if="!isNaN(weather.temp_feels)">Feels Like: {{ Math.round(weather.temp_feels) }}°F</span>
-    <span>{{ weather.condition }}</span>
+    <span v-if="weather.condition">{{ weather.condition }}</span>
     <img :src="weather.icon" alt="" v-if="weather.icon" />
+    <span v-if="!isNaN(weather.temp_f)">Current Temp: {{ Math.round(weather.temp_f) }}°F</span>
+    <span v-if="!isNaN(weather.feelslike_f)">Feels Like: {{ Math.round(weather.feelslike_f) }}°F</span>
+    <span v-if="!isNaN(weather.humidity)">Humidity: {{ Math.round(weather.humidity) }}%</span>
+    <span v-if="!isNaN(weather.precip_mm)">Rain (in mm): {{ Math.round(weather.precip_mm) }}mm</span>
+
+
   </div>
   <span v-else>Loading weather…</span>
 </template>
